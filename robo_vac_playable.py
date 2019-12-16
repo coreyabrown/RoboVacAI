3e476ea92719c7a158418969014491a6538775bb
Corey <coreyabrown.cb@gmail.com>
2019-12-16 11:55:23 -0600

    Finish base game.

--- robo_vac_playable.py
+++ robo_vac_playable.py
@@ -37,18 +37,15 @@ def play():
 
         # Scoring and timing
         passed_time = pygame.time.get_ticks() - start_time
-        pygame.draw.rect(env.background, black, (10, 10, 260, 150))
+        pygame.draw.rect(env.background, black, (10, 10, 260, 120))
         time = font.render('Time: ' + str(round(passed_time / 1000, 2)), True, font_color)
         env.background.blit(time, (20, 20))
         cleaned = font.render('Clean: ' + str(env.calc_clean_percent()), True, font_color)
         env.background.blit(cleaned, (20, 70))
-        score = round((env.calc_clean_percent() * 100) ** 2 / ((passed_time / 1000) + .01), 2)
-        score_display = font. render('Score: ' + str(score), True, font_color)
-        env.background.blit(score_display, (20, 120))
 
         if env.calc_clean_percent() >= .9 or passed_time / 1000 >= 300:
-            print('Game Over. You scored ' + str(score) + ' points')
-            print('Clean percentage: ' + str(env.calc_clean_percent() * 100))
+            print('Game Over. It took you ' + str(round(passed_time / 1000, 2)) + ' seconds to clean ' +
+                  str(env.calc_clean_percent() * 100) + '%!')
             clean = True
 
         pygame.display.update()
